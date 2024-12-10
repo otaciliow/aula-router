82e68c26b622fd7ad3d01ccf8531db06b7e95f0f
otaciliow <otacilio.dml@hotmail.com>
2024-12-09 22:10:27 -0300

    feat: added useParams

--- src/pages/produto/index.tsx
+++ src/pages/produto/index.tsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom"
+
 export function Produto() {
+
+    const { id } = useParams();
+
     return (
         <div>
-            <h1>Este é o produto </h1>
+            <h1>Este é o produto {id}</h1>
         </div>
     )
 }
